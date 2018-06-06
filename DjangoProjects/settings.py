c6b4cdc790de0e055c22af6a73f7e2ca19098d25
kpahwani <kaushal.pahwani@gmail.com>
2018-06-06 20:49:31 +0530

    Implemented below: Secondary email support

--- DjangoProjects/settings.py
+++ DjangoProjects/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+*3%q-0y2k9^9u8gnl@qv%ao^0^$99zmwm(@k(0a$0)%)88gv)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
